258243831f81c982539ec53ce0a429b399c56430
Igor Doslov <s191719@students.aie.edu.au>
2021-06-04 16:08:04 +1000

    hitting targets WIP

--- Team6 VR/Assets/Scripts/Igor Scripts/HitLocation.cs
+++ Team6 VR/Assets/Scripts/Igor Scripts/HitLocation.cs
@@ -1,12 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HitLocation : MonoBehaviour
 {
-    public float Outer = 0f;
-    public float Middle = 0f;
-    public float Centre = 0f;
+    public GameObject floatingScore;
+    public float outer = 0f;
+    public float middle = 0f;
+    public float bullseye = 0f;
+    public string outerScore = "100";
+    public string middleScore = "200";
+    public string bullseyeScore = "500";
+
 
     void OnCollisionEnter(Collision other)
     {
@@ -14,19 +18,30 @@ public class HitLocation : MonoBehaviour
         print("First point that collided: " + other.contacts[0].point);
         float dis = Vector3.Distance(other.contacts[0].point, transform.position);
         Debug.Log(dis);
-        if(dis > Outer)
+
+        if(dis > outer)
         {
+            ShowScoreText(outerScore, other);
             Debug.Log("hit outer");
         }
-        else if (dis > Middle)
+        else if (dis > middle)
         {
+            ShowScoreText(middleScore, other);
+
             Debug.Log("hit middle");
 
         }
-        else if(dis > Centre)
+        else if(dis > bullseye)
         {
+            ShowScoreText(bullseyeScore, other);
             Debug.Log("bullseye");
 
         }
+    }
+
+    public void ShowScoreText(string score, Collision other)
+    {
+        GameObject points = Instantiate(floatingScore, other.contacts[0].point, Quaternion.identity);
+        points.transform.GetChild(0).GetComponent<TextMeshPro>().text = score;
     }
 }
