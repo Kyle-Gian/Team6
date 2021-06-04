1fd89bcb61f7e31e33e4c49fd4f462b1b3f4268b
Kyle Gian <kyle.gian@hotmail.com>
2021-06-04 16:43:43 +1000

    Changes made to clipboard

---
+++ Team6 VR/Assets/Scripts/Kyle Scripts/Challenges.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+[System.Serializable]
+public class Challenges
+{
+    public string _challenge;
+    public bool _challengeComplete;
+}
