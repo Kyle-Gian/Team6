b12c0bda0ecca8fe49cf106b3648d9c25b8f56af
Igor Doslov <s191719@students.aie.edu.au>
2021-06-04 12:27:38 +1000

    Igor test scene shooting now works

---
+++ Team6 VR/Assets/Scripts/Igor Scripts/Player.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 10;
+    protected Vector3 moveVec = Vector3.zero; // movement speed of player
+    protected Rigidbody rigidbody;			// reference to the rigidbody
+    public GameObject projectile;
+    public Transform gunEnd;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+		rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        UpdateWhenGrounded();
+    }
+
+    void UpdateWhenGrounded()
+    {
+        // 1 
+        rigidbody.velocity = moveVec * moveSpeed;
+
+        // 2
+        if (moveVec != Vector3.zero)
+        {
+            transform.LookAt(this.transform.position + moveVec.normalized);
+        }
+
+        // 3
+        //CheckShouldFall();
+    }
+
+    public void OnMove(InputValue input)
+    {
+        Vector2 inputVec = input.Get<Vector2>();
+
+        moveVec = new Vector3(inputVec.x, 0, inputVec.y);
+    }
+
+    public void OnShoot()
+    {
+        Ray mouseRay = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        GameObject newGameObject = (GameObject)Instantiate(projectile, mouseRay.origin, Quaternion.identity);
+        Rigidbody rb = newGameObject.GetComponent<Rigidbody>();
+
+        if (rb != null)
+        {
+            rb.velocity = mouseRay.direction * moveSpeed;
+        }
+    }
+}
