37cc7b62428629ff95dad66146e6f4d98a7be0b8
Kyle Gian <kyle.gian@hotmail.com>
2021-06-05 21:26:04 +1000

    Text added to clipboard text from script

--- Team6 VR/Assets/Scripts/Kyle Scripts/ClipBoard.cs
+++ Team6 VR/Assets/Scripts/Kyle Scripts/ClipBoard.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ClipBoard : MonoBehaviour
 {
-    [SerializeField] TextMesh _paperText;
+    [SerializeField] GameObject _paperText;
     [SerializeField] List<Challenges> _challenges = new List<Challenges>();
 
+    private List<TextMeshPro> _textList = new List<TextMeshPro>();
+    
     // Start is called before the first frame update
     void Start()
     {
+        if (_paperText != null)
+        {
+           _textList = _paperText.GetComponentsInChildren<TextMeshPro>().ToList();
+        }
+
+        for (int i = 0; i < _textList.Count; i++)
+        {
+            _textList[i].text = _challenges[i]._challenge;
+
+        }
     }
 
     // Update is called once per frame
