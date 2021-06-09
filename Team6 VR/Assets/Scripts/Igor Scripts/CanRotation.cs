45b0a13df0405ac6e2ca3a309d89ab0ba3b1ff84
Igor Doslov <s191719@students.aie.edu.au>
2021-06-09 13:58:22 +1000

    Can rotation score WIP

--- Team6 VR/Assets/Scripts/Igor Scripts/CanRotation.cs
+++ Team6 VR/Assets/Scripts/Igor Scripts/CanRotation.cs
@@ -11,28 +11,81 @@ public class CanRotation : MonoBehaviour
     [SerializeField]
     float eulerAngleZ;
 
+    public List<Transform> cans1 = new List<Transform>();
+    public List<Transform> cans2 = new List<Transform>();
+    public List<Transform> cans3 = new List<Transform>();
+    public List<Transform> cans4 = new List<Transform>();
 
-    void Update()
+    public Vector3 eulerAngles;
+
+    ScoreScreen ss;
+
+    public int scoreValue;
+
+    private void Start()
     {
+        ss = FindObjectOfType<ScoreScreen>();
+        eulerAngles = transform.localEulerAngles;
 
-        eulerAngleX = transform.localEulerAngles.x;
-        eulerAngleY = transform.localEulerAngles.y;
-        eulerAngleZ = transform.localEulerAngles.z;
-
-        if (eulerAngleX > 45f && eulerAngleX < 315f || eulerAngleZ > 45f && eulerAngleZ < 315f)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            Debug.Log("fall");
+            Transform canStack = transform.GetChild(i);
+
+            for (int j = 0; j < canStack.childCount; j++)
+            {
+                if (i == 0)
+                {
+                    cans1.Add(canStack.GetChild(j));
+
+                }
+                else if (i == 1)
+                {
+                    cans2.Add(canStack.GetChild(j));
+
+                }
+                else if (i == 2)
+                {
+                    cans3.Add(canStack.GetChild(j));
+
+                }
+                else if (i == 3)
+                {
+                    cans4.Add(canStack.GetChild(j));
+
+                }
+            }
         }
+
+    }
+
+    private void Update()
+    {
+        foreach (var c in cans1)
+        {
+            CanRotationCheck(c);
+        }
+
+
     }
 
     //private void OnCollisionEnter(Collision other)
     //{
     //    if (other.gameObject.CompareTag("Ball"))
     //    {
-    //        if(eulerAngleX > 45f || eulerAngleX < 315f)
+    //        if (eulerAngles.x > 45f && eulerAngles.x < 315f || eulerAngles.z > 45f && eulerAngles.z < 315f)
     //        {
-
+    //            ss.score += scoreValue;
     //        }
     //    }
     //}
+
+    public void CanRotationCheck(Transform can)
+    {
+        if (can.eulerAngles.x > 45f && can.eulerAngles.x < 315f || can.eulerAngles.z > 45f && can.eulerAngles.z < 315f)
+        {
+            
+            ss.score += scoreValue;
+            can.GetComponent<CanSelfData>().fallen = true;
+        }
+    }
 }
