1cf858a0cc8d940faa3d01d3ea93b2110a29da28
Kyle Gian <kyle.gian@hotmail.com>
2021-06-11 12:40:58 +1000

    Adding new scene and change to prefab

---
+++ Team6 VR/Assets/Scripts/Kyle Scripts/Challenges/SmallTargetsKnocked.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallTargetsKnocked : Challenges
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void ChallengeIsCompleted()
+    {
+        _challengeComplete = true;
+    }
+}
