a790eb121708160ea479f2fabda3bbc7a5b98495
Kyle Gian <kyle.gian@hotmail.com>
2021-06-16 11:11:01 +1000

    Auto stash before rebase of "origin/main"

---
+++ Team6 VR/Assets/Scripts/Kyle Scripts/HandSelectionController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandSelectionController : XRGrabInteractable
+{
+    
+    //Stops the object from being grabbed by 2 hands
+    public override bool IsSelectableBy(XRBaseInteractor interactor)
+    {
+        bool isAlreadyGrabbed = selectingInteractor && !interactor.Equals(selectingInteractor);
+        
+        return base.IsSelectableBy(interactor) && !isAlreadyGrabbed;
+    }
+}
