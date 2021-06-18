0fada6ad047353c057ac9146c88187942ead4b21
Igor Doslov <s191719@students.aie.edu.au>
2021-06-18 16:49:19 +1000

    Dart projectile changed to XRInteractable

--- Team6 VR/Assets/Scripts/Igor Scripts/DartProjectile.cs
+++ Team6 VR/Assets/Scripts/Igor Scripts/DartProjectile.cs
@@ -4,8 +4,9 @@
 
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class DartProjectile : MonoBehaviour
+public class DartProjectile : XRGrabInteractable
 {
 
     public AudioClip impact;
@@ -16,7 +17,9 @@ public class DartProjectile : MonoBehaviour
     public UnityEvent ProjectileHit;
     public bool hitSomething = false;
     public bool soundHasPlayed = false;
+    public bool hasBeenThrown = false;
 
+    ShootFromGun sfg;
 
     private void Start()
     {
@@ -24,42 +27,72 @@ public class DartProjectile : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         transform.rotation = Quaternion.LookRotation(rb.velocity);
 
+        sfg = FindObjectOfType<ShootFromGun>();
     }
 
     private void FixedUpdate()
     {
-        if (!hitSomething)
+
+        if (!hitSomething || isSelected)
+        { 
+            rb.constraints = RigidbodyConstraints.None;
             transform.rotation = Quaternion.LookRotation(rb.velocity); // Makes the dart rotate according to its velocity
+
+        }
+
+        if (sfg._weaponShot)
+        {
+            rb.constraints = RigidbodyConstraints.None;
+            transform.rotation = Quaternion.LookRotation(rb.velocity); // Makes the dart rotate according to its velocity
+        }
+
     }
 
     void OnCollisionEnter(Collision other)
     {
-
-        if (!soundHasPlayed) // Prevents sound playing more than once when dart is stuck to object
+        if (sfg._weaponShot || hasBeenThrown)
         {
-            audioSource.PlayOneShot(impact);
-            soundHasPlayed = true;
-
-        }
-
-        if (!other.gameObject.CompareTag("Gun"))
-        {
-            hitSomething = true;
-            Stick(other);
-            // If a can or popUpTarget then parent so the dart moves with the object
-            if (other.gameObject.CompareTag("Can") || other.gameObject.CompareTag("PopUpTarget"))
+            if (!soundHasPlayed) // Prevents sound playing more than once when dart is stuck to object
             {
-                transform.SetParent(other.transform);
+                audioSource.PlayOneShot(impact);
+                soundHasPlayed = true;
 
             }
 
+            if (!other.gameObject.CompareTag("Gun"))
+            {
+                hitSomething = true;
+                Stick(other);
+                // If a can or popUpTarget then parent so the dart moves with the object
+                if (other.gameObject.CompareTag("Can") || other.gameObject.CompareTag("PopUpTarget"))
+                {
+                    transform.SetParent(other.transform);
+
+                }
+
+            }
+            hasBeenThrown = false;
         }
 
     }
-    
+
     // Freezes x, y, z position and rotation values
     private void Stick(Collision other)
     {
         rb.constraints = RigidbodyConstraints.FreezeAll;
     }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        hasBeenThrown = true;
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        transform.SetParent(null,true);
+      
+    }
+
 }
