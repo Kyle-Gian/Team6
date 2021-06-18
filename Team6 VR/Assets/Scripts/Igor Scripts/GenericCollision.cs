21c72db71b20d13470ca658f4b7b2f38c64bda7e
Igor Doslov <s191719@students.aie.edu.au>
2021-06-18 15:30:58 +1000

    Generic collision script tweaked

--- Team6 VR/Assets/Scripts/Igor Scripts/GenericCollision.cs
+++ Team6 VR/Assets/Scripts/Igor Scripts/GenericCollision.cs
@@ -10,18 +10,9 @@ public class GenericCollision : MonoBehaviour
 {
     public UnityEvent onCollisionEvent;
 
-    private void Start()
-    {
-        //onCollisionEvent = new UnityEvent();
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         onCollisionEvent.Invoke();
     }
 
-    private void OnCollisionExit(Collision collision)
-    {
-        onCollisionEvent.Invoke();
-    }
 }
