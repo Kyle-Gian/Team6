463f0133a20768fb4a9c8bfaf24116fda1342755
Kyle Gian <kyle.gian@hotmail.com>
2021-06-26 16:46:34 +1000

    Potential fix for gun respawn

--- Team6 VR/Assets/Scripts/Kyle Scripts/Interactables/GunResetPosition.cs
+++ Team6 VR/Assets/Scripts/Kyle Scripts/Interactables/GunResetPosition.cs
@@ -7,14 +7,9 @@ public class GunResetPosition : MonoBehaviour
 {
    private Rigidbody _rigidbody;
     private Vector3 _startPos;
-    [SerializeField] private float _waitTime = 5;
-    private bool _inAction;
+    [SerializeField] private float _waitTime = 2;
+    private bool _inHand;
     private Quaternion _startRotation;
-    
-    public GameObject _leftHand;
-    public GameObject _rightHand;
-    XRDirectInteractor _leftHandInteractor;
-    XRDirectInteractor _rightHandInteractor;
 
     // Start is called before the first frame update
     void Start()
@@ -22,59 +17,25 @@ public class GunResetPosition : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
         _startPos = transform.position;
         _startRotation = Quaternion.identity;
-        _leftHand = GameObject.FindWithTag("left");
-        _rightHand = GameObject.FindWithTag("right");
-
-        //If either the left or right hand are null throw an error for the user
-        if (_leftHand != null)
-        {
-            //Get the left hand interactor and access the event
-            _leftHandInteractor = _leftHand.GetComponent<XRDirectInteractor>();
-            _leftHandInteractor.selectEntered.AddListener(ObjectInAction);
-            _leftHandInteractor.selectExited.AddListener(ObjectNotInAction);
-
-
-        }
-        else
-        {
-            Debug.LogWarning("Left hand has not been attached, Check Tag on left hand controller");
-
-        }
-        if (_rightHand != null)
-        {
-            //Get the right hand interactor and access the event
-            _rightHandInteractor = _rightHand.GetComponent<XRDirectInteractor>();
-            _rightHandInteractor.selectEntered.AddListener(ObjectInAction);
-            _rightHandInteractor.selectExited.AddListener(ObjectNotInAction);
-
-
-        }
-        else
-        {
-            Debug.LogWarning("Right hand has not been attached, Check Tag on Right hand controller");
-
-        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(_inAction);
         //Check the distance from start position, will trigger respawn after set time
-        if (Vector3.Distance(transform.position, _startPos) > .5f)
+        if (Vector3.Distance(transform.position, _startPos) > 0.6f)
         {
-            if (!_inAction)
+            if (!_inHand)
             {
                 StartCoroutine("RespawnTime");
             }
-
         }
     }
 
     public void ResetObjectPos()
     {
-        if (!_inAction)
+        if (!_inHand)
         {
             gameObject.SetActive(false);
             _rigidbody.velocity = Vector3.zero;
@@ -92,12 +53,9 @@ public class GunResetPosition : MonoBehaviour
         ResetObjectPos();
     }
 
-    public void ObjectInAction(SelectEnterEventArgs test)
+    public void InHand(bool isInhand)
     {
-        _inAction = true;
+        _inHand = isInhand;
     }
-    public void ObjectNotInAction(SelectExitEventArgs test)
-    {
-        _inAction = false;
-    }
+
 }
